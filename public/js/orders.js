7739dd24e93d4129bd5fbed242b70d8c9070e751
Chiara <124071052+chiarabis@users.noreply.github.com>
2024-10-03 09:57:13 +0200

    Add files via upload

--- public/js/orders.js
+++ public/js/orders.js
@@ -28,21 +28,54 @@ function deleteOrder(orderId, orderDiv) {
 };
 
 //modifica ordine
-/*function editOrder(order) {
+function editOrder(order) {
+    const orderIdInput = document.createElement('input');
+    const nameInput = document.getElementById('name');
+    const destinationInput = document.getElementById('destination');
 
     orderForm.appendChild(orderIdInput);
     orderForm.scrollIntoView();
-};*/
+};
 
 
 function updateOrderDiv(order, orderDiv){
     orderDiv.innerHTML = `
-        <h3>Order number ${order.id}</h3>
-        <div>
-            <span>date ${order.createdAt}</span>
-            <span>user ${order.userId}</span>
+        <div style="display: flex; flex-direction: row; justify-content: space-between;">
+            <h3>Order #${order.id}</h3>
+
+            <div class="containerBtn" style="align-items: flex-end;">
+                <div class="containerBtn" style="width: 60px;">
+                    <button class='delete iconBtn' style='margin:0'><img alt='Delete' src='./delete.png'/></button>
+                    <button class='edit iconBtn' style='margin:0'><img alt='Edit' src='./edit.png'/></button>
+                </div>
+            </div>
         </div>
-    `
+
+        <div class="orderInfo">
+            <span>Date ${order.createdAt}</span>
+            <span>User: ${order.User.firstname} ${order.User.lastname}</span>
+        </div>
+    `;
+
+    const productList = document.createElement('div');
+    productList.classList.add('productList');
+
+    order.Products.forEach(product => {
+        const productName = document.createElement('p');
+        productName.textContent = product.name;
+        productList.appendChild(productName);
+    });
+    orderDiv.appendChild(productList);
+
+    const deleteBtn = orderDiv.querySelector('.delete');
+    const editBtn = orderDiv.querySelector('.edit');
+    
+    deleteBtn.addEventListener('click', function() {
+        deleteOrder(order.id, orderDiv);
+    });
+    editBtn.addEventListener('click', function() {
+        editOrder(order);
+    });
 }
 
 
